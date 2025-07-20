17a362f4e709e56208e4b8ce5561ce7b6193d252
David Mendez <davidmendezrosa@gmail.com>
2025-07-19 22:56:32 -0400

    attempt to fix pfp

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   output: 'export',
   basePath: '/Personal-Portfolio',
   assetPrefix: '/Personal-Portfolio/',
+  distDir: 'docs',
   eslint: {
     ignoreDuringBuilds: true,
   },
